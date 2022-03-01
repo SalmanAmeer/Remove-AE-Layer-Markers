311d27a6e56fb99d7c48f95749d03aeda4b12b89
Salman Ameer <59564333+SalmanAmeer@users.noreply.github.com>
2022-03-01 15:24:24 +0500

    Add files via upload

---
+++ Remove Marker in AE.jsx
@@ -0,0 +1,12 @@
+﻿
+app.beginUndoGroup("Remove Markers");
+for(var i =0; i < app.project.activeItem.selectedLayers.length; i++){
+        thisLayer = app.project.activeItem.selectedLayers[i];
+        
+                for(var j=1; j<= thisLayer.marker.numKeys; j++){
+                    currentMarkerRemove = thisLayer.marker.removeKey(j);
+                    j = j-1;
+                    };
+    
+    };
+app.endUndoGroup();
